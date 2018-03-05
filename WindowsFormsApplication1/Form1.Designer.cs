044f2e7b26e98ad88bd9a6d2736756cb6d551909
BuiNguyenHoangAnh <32384308+BuiNguyenHoangAnh@users.noreply.github.com>
2018-03-05 12:49:55 +0700

    First commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_start = new System.Windows.Forms.Button();
+            this.bt_exit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bt_start
+            // 
+            this.bt_start.Location = new System.Drawing.Point(13, 226);
+            this.bt_start.Name = "bt_start";
+            this.bt_start.Size = new System.Drawing.Size(75, 23);
+            this.bt_start.TabIndex = 0;
+            this.bt_start.Text = "start";
+            this.bt_start.UseVisualStyleBackColor = true;
+            this.bt_start.Click += new System.EventHandler(this.bt_start_Click);
+            // 
+            // bt_exit
+            // 
+            this.bt_exit.Location = new System.Drawing.Point(197, 226);
+            this.bt_exit.Name = "bt_exit";
+            this.bt_exit.Size = new System.Drawing.Size(75, 23);
+            this.bt_exit.TabIndex = 1;
+            this.bt_exit.Text = "Exit";
+            this.bt_exit.UseVisualStyleBackColor = true;
+            this.bt_exit.Click += new System.EventHandler(this.bt_exit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Location = new System.Drawing.Point(57, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(52, 50);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Location = new System.Drawing.Point(115, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(52, 50);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox3.Location = new System.Drawing.Point(173, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(52, 50);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox4.Location = new System.Drawing.Point(57, 68);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(52, 50);
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox5.Location = new System.Drawing.Point(115, 68);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(52, 50);
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox6.Location = new System.Drawing.Point(173, 68);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(52, 50);
+            this.pictureBox6.TabIndex = 7;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox7.Location = new System.Drawing.Point(57, 124);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(52, 50);
+            this.pictureBox7.TabIndex = 8;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox8.Location = new System.Drawing.Point(115, 124);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(52, 50);
+            this.pictureBox8.TabIndex = 9;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox9.Location = new System.Drawing.Point(173, 124);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(52, 50);
+            this.pictureBox9.TabIndex = 10;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "score:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(140, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 18);
+            this.label2.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Mistral", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(52, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(175, 48);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "ĐÚNG RỒI";
+            this.label3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.bt_exit);
+            this.Controls.Add(this.bt_start);
+            this.Name = "Form1";
+            this.Text = "chon mau";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_start;
+        private System.Windows.Forms.Button bt_exit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int temp, diem = 0;
+
+        private void bt_exit_Click(object sender, EventArgs e)
+        {
+            DialogResult rp = MessageBox.Show("Are you sure?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (rp == DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void bt_start_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            temp = r.Next(1, 9);
+            switch(temp)
+            {
+                case 1:
+                    {
+                        this.pictureBox1.BackColor = SystemColors.Highlight;
+                        this.pictureBox2.BackColor = SystemColors.HotTrack;
+                        this.pictureBox3.BackColor = SystemColors.HotTrack;
+                        this.pictureBox4.BackColor = SystemColors.HotTrack;
+                        this.pictureBox5.BackColor = SystemColors.HotTrack;
+                        this.pictureBox6.BackColor = SystemColors.HotTrack;
+                        this.pictureBox7.BackColor = SystemColors.HotTrack;
+                        this.pictureBox8.BackColor = SystemColors.HotTrack;
+                        this.pictureBox9.BackColor = SystemColors.HotTrack;
+                        break;
+                    }
+                case 2:
+                    {
+                        this.pictureBox1.BackColor = SystemColors.HotTrack;
+                        this.pictureBox2.BackColor = SystemColors.Highlight;
+                        this.pictureBox3.BackColor = SystemColors.HotTrack;
+                        this.pictureBox4.BackColor = SystemColors.HotTrack;
+                        this.pictureBox5.BackColor = SystemColors.HotTrack;
+                        this.pictureBox6.BackColor = SystemColors.HotTrack;
+                        this.pictureBox7.BackColor = SystemColors.HotTrack;
+                        this.pictureBox8.BackColor = SystemColors.HotTrack;
+                        this.pictureBox9.BackColor = SystemColors.HotTrack;
+                        break;
+                    }
+                case 3:
+                    {
+                        this.pictureBox1.BackColor = SystemColors.HotTrack;
+                        this.pictureBox2.BackColor = SystemColors.HotTrack;
+                        this.pictureBox3.BackColor = SystemColors.Highlight;
+                        this.pictureBox4.BackColor = SystemColors.HotTrack;
+                        this.pictureBox5.BackColor = SystemColors.HotTrack;
+                        this.pictureBox6.BackColor = SystemColors.HotTrack;
+                        this.pictureBox7.BackColor = SystemColors.HotTrack;
+                        this.pictureBox8.BackColor = SystemColors.HotTrack;
+                        this.pictureBox9.BackColor = SystemColors.HotTrack;
+                        break;
+                    }
+                case 4:
+                    {
+                        this.pictureBox1.BackColor = SystemColors.HotTrack;
+                        this.pictureBox2.BackColor = SystemColors.HotTrack;
+                        this.pictureBox3.BackColor = SystemColors.HotTrack;
+                        this.pictureBox4.BackColor = SystemColors.Highlight;
+                        this.pictureBox5.BackColor = SystemColors.HotTrack;
+                        this.pictureBox6.BackColor = SystemColors.HotTrack;
+                        this.pictureBox7.BackColor = SystemColors.HotTrack;
+                        this.pictureBox8.BackColor = SystemColors.HotTrack;
+                        this.pictureBox9.BackColor = SystemColors.HotTrack;
+                        break;
+                    }
+                case 5:
+                    {
+                        this.pictureBox1.BackColor = SystemColors.HotTrack;
+                        this.pictureBox2.BackColor = SystemColors.HotTrack;
+                        this.pictureBox3.BackColor = SystemColors.HotTrack;
+                        this.pictureBox4.BackColor = SystemColors.HotTrack;
+                        this.pictureBox5.BackColor = SystemColors.Highlight;
+                        this.pictureBox6.BackColor = SystemColors.HotTrack;
+                        this.pictureBox7.BackColor = SystemColors.HotTrack;
+                        this.pictureBox8.BackColor = SystemColors.HotTrack;
+                        this.pictureBox9.BackColor = SystemColors.HotTrack;
+                        break;
+                    }
+                case 6:
+                    {
+                        this.pictureBox1.BackColor = SystemColors.HotTrack;
+                        this.pictureBox2.BackColor = SystemColors.HotTrack;
+                        this.pictureBox3.BackColor = SystemColors.HotTrack;
+                        this.pictureBox4.BackColor = SystemColors.HotTrack;
+                        this.pictureBox5.BackColor = SystemColors.HotTrack;
+                        this.pictureBox6.BackColor = SystemColors.Highlight;
+                        this.pictureBox7.BackColor = SystemColors.HotTrack;
+                        this.pictureBox8.BackColor = SystemColors.HotTrack;
+                        this.pictureBox9.BackColor = SystemColors.HotTrack;
+                        break;
+                    }
+                case 7:
+                    {
+                        this.pictureBox1.BackColor = SystemColors.HotTrack;
+                        this.pictureBox2.BackColor = SystemColors.HotTrack;
+                        this.pictureBox3.BackColor = SystemColors.HotTrack;
+                        this.pictureBox4.BackColor = SystemColors.HotTrack;
+                        this.pictureBox5.BackColor = SystemColors.HotTrack;
+                        this.pictureBox6.BackColor = SystemColors.HotTrack;
+                        this.pictureBox7.BackColor = SystemColors.Highlight;
+                        this.pictureBox8.BackColor = SystemColors.HotTrack;
+                        this.pictureBox9.BackColor = SystemColors.HotTrack;
+                        break;
+                    }
+                case 8:
+                    {
+                        this.pictureBox1.BackColor = SystemColors.HotTrack;
+                        this.pictureBox2.BackColor = SystemColors.HotTrack;
+                        this.pictureBox3.BackColor = SystemColors.HotTrack;
+                        this.pictureBox4.BackColor = SystemColors.HotTrack;
+                        this.pictureBox5.BackColor = SystemColors.HotTrack;
+                        this.pictureBox6.BackColor = SystemColors.HotTrack;
+                        this.pictureBox7.BackColor = SystemColors.HotTrack;
+                        this.pictureBox8.BackColor = SystemColors.Highlight;
+                        this.pictureBox9.BackColor = SystemColors.HotTrack;
+                        break;
+                    }
+                case 9:
+                    {
+                        this.pictureBox1.BackColor = SystemColors.HotTrack;
+                        this.pictureBox2.BackColor = SystemColors.HotTrack;
+                        this.pictureBox3.BackColor = SystemColors.HotTrack;
+                        this.pictureBox4.BackColor = SystemColors.HotTrack;
+                        this.pictureBox5.BackColor = SystemColors.HotTrack;
+                        this.pictureBox6.BackColor = SystemColors.HotTrack;
+                        this.pictureBox7.BackColor = SystemColors.HotTrack;
+                        this.pictureBox8.BackColor = SystemColors.HotTrack;
+                        this.pictureBox9.BackColor = SystemColors.Highlight;
+                        break;
+                    }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (temp == 1)
+            {
+                diem = diem + 10;
+                bt_start_Click(sender, e);
+                this.label2.Text = Convert.ToString(diem);
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (temp == 2)
+            {
+                diem = diem + 10;
+                bt_start_Click(sender, e);
+                this.label2.Text = Convert.ToString(diem);
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            if (temp == 3)
+            {
+                diem = diem + 10;
+                bt_start_Click(sender, e);
+                this.label2.Text = Convert.ToString(diem);
+            }
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            if (temp == 4)
+            {
+                diem = diem + 10;
+                bt_start_Click(sender, e);
+                this.label2.Text = Convert.ToString(diem);
+            }
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            if (temp == 5)
+            {
+                diem = diem + 10;
+                bt_start_Click(sender, e);
+                this.label2.Text = Convert.ToString(diem);
+            }
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            if (temp == 6)
+            {
+                diem = diem + 10;
+                bt_start_Click(sender, e);
+                this.label2.Text = Convert.ToString(diem);
+            }
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            if (temp == 7)
+            {
+                diem = diem + 10;
+                bt_start_Click(sender, e);
+                this.label2.Text = Convert.ToString(diem);
+            }
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            if (temp == 8)
+            {
+                diem = diem + 10;
+                bt_start_Click(sender, e);
+                this.label2.Text = Convert.ToString(diem);
+            }
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            if (temp == 9)
+            {
+                diem = diem + 10;
+                bt_start_Click(sender, e);
+                this.label2.Text = Convert.ToString(diem);
+            }
+        }
+    }
+}
